ef87920b17337153f401ba855f73bb68e58cb66c
Brayden <braydenh51@gmail.com>
2019-02-12 02:11:34 +0000

    avatar downloader connected

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secrets = require('./secrets.js')
+var fs = require('fs');
 
 
 
@@ -18,15 +19,32 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, function(err, res, body) {
     cb(err, JSON.parse(body));
   });
-}
-
+}// set vartaBLE TO AVURL AND FILEPATH
+var url1 = ''
+var filep = 'avatars/'
 getRepoContributors("jquery", "jquery", function(err, result) {
-  let output = []
   let avurl = result
       for(let i = 0; i < result.length; i++) {
-        console.log(avurl[i].avatar_url)
+        url1 += avurl[i].avatar_url
+        filep += avurl[i].login + '.jpg'
 
+
+        downloadImageByURL(url1, filep)
+        url1 = ''
+        filep = 'avatars/'
+       // console.log(avurl[i].avatar_url)
+// SET AVURL AND SET FILEPATH
       }
  // console.log("Errors:", err);
  // console.log("Result:", avurl);
 });
+function downloadImageByURL(url, filePath) {
+  // ...
+  request.get(url)
+  .on('error', function (err) {
+         throw err;
+  })
+
+  .pipe(fs.createWriteStream(filePath));
+}
+//downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
