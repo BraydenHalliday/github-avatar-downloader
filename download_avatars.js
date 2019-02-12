42d1cf0da05e413719564911a1f247369f961abf
Brayden <braydenh51@gmail.com>
2019-02-12 01:26:01 +0000

    av_url update

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,11 @@
 var request = require('request');
 var secrets = require('./secrets.js')
 
+
+
+
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -11,11 +16,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(err, JSON.parse(body));
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  let output = []
+  let avurl = result
+      for(let i = 0; i < result.length; i++) {
+        console.log(avurl[i].avatar_url)
+
+      }
+ // console.log("Errors:", err);
+ // console.log("Result:", avurl);
 });
